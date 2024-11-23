3a4de4a47b5227f1335a5e5b4d311cf6a2b55f72
asianirish <asianirish@gmail.com>
2024-11-23 22:03:49 +0100

    feat: implement error and result handling methods

--- cmd/Menu.cpp
+++ cmd/Menu.cpp
@@ -9,6 +9,16 @@ Menu::Menu() {}
 void Menu::registerCommand(const std::string &commandName, const Value &actionInfo)
 {
     commands_.insert({commandName, actionInfo});
+
+    {
+        auto f = std::bind(&Menu::onTaskComplete, this, std::placeholders::_1);
+        taskManager_.setCallback(f);
+    }
+
+    {
+        auto f = std::bind(&Menu::onTaskError, this, std::placeholders::_1);
+        taskManager_.setErrorCallback(f);
+    }
 }
 
 const std::list<std::string> &Menu::commandList() const
@@ -31,6 +41,10 @@ void Menu::processString(const std::string &input)
 
 void Menu::execute(const std::string &command, const InputArgList &args)
 {
+    if (!commands_.contains(command)) {
+        errorCallback_("Command '" + command + "' not found.");
+        return;
+    }
     auto actionDesc = commands_.at(command);
     taskManager_.execute(actionDesc, args);
 }
@@ -43,6 +57,35 @@ std::shared_ptr<Translator> Menu::translator() const
 void Menu::setTranslator(const std::shared_ptr<Translator> &newTranslator)
 {
     translator_ = newTranslator;
+}
+
+TaskManager Menu::taskManager() const
+{
+    return taskManager_;
+}
+
+void Menu::setResultCallback(const ResultCallback &newResultCallback)
+{
+    resultCallback_ = newResultCallback;
+}
+
+void Menu::setErrorCallback(const ErrorCallback &newErrorCallback)
+{
+    errorCallback_ = newErrorCallback;
+}
+
+void Menu::onTaskComplete(const Value &result)
+{
+    if (resultCallback_) {
+        resultCallback_(result);
+    }
+}
+
+void Menu::onTaskError(const err::Error &error)
+{
+    if (errorCallback_) {
+        errorCallback_(error.description());
+    }
 }
 
 
--- cmd/Menu.h
+++ cmd/Menu.h
@@ -5,6 +5,7 @@
 #include "cmd/Types.h"
 #include "cmd/Value.h"
 #include "cmd/Translator.h"
+#include "cmd/err/Error.h"
 
 #include <map>
 #include <string>
@@ -16,6 +17,9 @@ namespace cmd {
 class Menu
 {
 public:
+    using ResultCallback = std::function<void(const std::string&)>;
+    using ErrorCallback = std::function<void(const std::string&)>;
+
     Menu();
 
     void registerCommand(const std::string &commandName, const Value &actionInfo);
@@ -30,6 +34,12 @@ public:
     std::shared_ptr<Translator> translator() const;
     void setTranslator(const std::shared_ptr<Translator> &newTranslator);
 
+    TaskManager taskManager() const;
+
+    void setResultCallback(const ResultCallback &newResultCallback);
+
+    void setErrorCallback(const ErrorCallback &newErrorCallback);
+
 private:
     std::map<std::string, Value> commands_;
     mutable std::list<std::string> commandList_;
@@ -37,6 +47,13 @@ private:
     TaskManager taskManager_;
 
     std::shared_ptr<Translator> translator_;
+
+    ResultCallback resultCallback_;
+    ErrorCallback errorCallback_;
+
+    void onTaskComplete(const Value &result);
+
+    void onTaskError(const err::Error &error);
 };
 
 } // namespace cmd
