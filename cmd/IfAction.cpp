d5aea219816b0d0c7a382cd4bd9c9ee493dd23e1
asianirish <asianirish@gmail.com>
2025-01-18 11:58:31 +0100

    feat: implement task argument definitions in actionDef

--- cmd/IfAction.cpp
+++ cmd/IfAction.cpp
@@ -13,13 +13,30 @@ def::ActionDef IfAction::actionDef() const
     ActionDef actionDef;
 
     const auto boolDef = TypeDef::createBoolTypedef();
-    // const auto taskDef = TypeDef::createTaskTypedef(); // TODO: implement Task TypeDef
+    const auto taskDef = TypeDef::createTaskTypedef();
 
-    ArgDef argDef;
-    argDef.setType(boolDef);
-    actionDef.addArgDef(argDef);
+    {
+        ArgDef argDef;
+        argDef.setType(boolDef);
+        argDef.setName("condition");
+        actionDef.addArgDef(argDef);
+    }
 
-    // TODO: implement
+    {
+        ArgDef argDef;
+        argDef.setType(taskDef);
+        argDef.setName("what if yes");
+        argDef.setMustBeCalculated(false);
+        actionDef.addArgDef(argDef);
+    }
+
+    {
+        ArgDef argDef;
+        argDef.setType(taskDef);
+        argDef.setName("what if no");
+        argDef.setMustBeCalculated(false);
+        actionDef.addArgDef(argDef);
+    }
 
     return actionDef;
 }
